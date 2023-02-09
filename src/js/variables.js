d9d4631d3814bb28bffe9c6e39fab9cc8079d320
ttkapostol <ttkapostolaki@gmail.com>
2023-02-09 16:46:11 +0100

    JS partials

---
+++ src/js/variables.js
@@ -0,0 +1,19 @@
+/* eslint-disable strict */
+
+const backgroundColor = document.querySelector('.js-background');
+const info = document.querySelector('.js-info');
+const socials = document.querySelector('.js-socials');
+
+const inputName = document.querySelector('.js-input-name');
+const inputJob = document.querySelector('.js-input-job');
+const inputNumber = document.querySelector('.js-input-number');
+const inputEmail = document.querySelector('.js-input-email');
+const inputLinkedin = document.querySelector('.js-input-linkedin');
+const inputGithub = document.querySelector('.js-input-github');
+
+const previewName = document.querySelector('.js-preview-name');
+const previewJob = document.querySelector('.js-preview-job');
+const linkNumber = document.querySelector('.js-preview-number');
+const linkEmail = document.querySelector('.js-preview-email');
+const linkLinkedin = document.querySelector('.js-preview-linkedin');
+const linkGithub = document.querySelector('.js-preview-github');
