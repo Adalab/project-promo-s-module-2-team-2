ed7d04e68138db32e54093348f7af02508397f0b
VanesaGR <vanesaglezrguez@gmail.com>
2023-02-10 13:39:41 +0100

    add js partials

--- src/js/01variables.js
+++ src/js/01variables.js
@@ -1,3 +1,41 @@
 /* eslint-disable strict */
 
+//VARIABLES COLLAPSABLES
 
+const designLegend = document.querySelector('.js-design-legend');
+const down1 = document.querySelector('.js-down1');
+const up1 = document.querySelector('.js-up1');
+const design = document.querySelector('.js-design');
+
+const fillLegend = document.querySelector('.js-fill-legend');
+const down2 = document.querySelector('.js-down2');
+const up2 = document.querySelector('.js-up2');
+const fill = document.querySelector('.js-fill');
+
+const shareLegend = document.querySelector('.js-share-legend');
+const down3 = document.querySelector('.js-down3');
+const up3 = document.querySelector('.js-up3');
+const share = document.querySelector('.js-share');
+
+//VARIABLES PALETTES
+const background = document.querySelector('.js-background');
+const info = document.querySelector('.js-info');
+const socials = document.querySelector('.js-socials');
+const radio1 = document.querySelector('.js-palette1');
+const radio2 = document.querySelector('.js-palette2');
+const radio3 = document.querySelector('.js-palette3');
+
+//VARIABLES INPUT-PREVIEW
+const inputName = document.querySelector('.js-input-name');
+const inputJob = document.querySelector('.js-input-job');
+const inputNumber = document.querySelector('.js-input-number');
+const inputEmail = document.querySelector('.js-input-email');
+const inputLinkedin = document.querySelector('.js-input-linkedin');
+const inputGithub = document.querySelector('.js-input-github');
+
+const previewName = document.querySelector('.js-preview-name');
+const previewJob = document.querySelector('.js-preview-job');
+const linkNumber = document.querySelector('.js-preview-number');
+const linkEmail = document.querySelector('.js-preview-email');
+const linkLinkedin = document.querySelector('.js-preview-linkedin');
+const linkGithub = document.querySelector('.js-preview-github');
--- src/js/02palettes.js
+++ src/js/02palettes.js
@@ -1,1 +1,40 @@
 
+
+//FUNCIONES PALETTES
+function handleRadio1(event) {
+  console.log('fiestaaaaa');
+  if (background.classList.contains('palette3') && info.classList.contains('palette3') && socials.classList.contains('palette3')) {
+    background.classList.remove('palette3');
+    info.classList.remove('palette3');
+    socials.classList.remove('palette3');
+  }
+  if (background.classList.contains('palette2') && info.classList.contains('palette2') && socials.classList.contains('palette2')) {
+    background.classList.remove('palette2');
+    info.classList.remove('palette2');
+    socials.classList.remove('palette2');
+  }
+}
+
+function handleRadio2(event) {
+  console.log('esperaaaaaanza');
+  background.classList.add('palette2');
+  info.classList.add('palette2');
+  socials.classList.add('palette2');
+  if (background.classList.contains('palette3') && info.classList.contains('palette3') && socials.classList.contains('palette3')) {
+    background.classList.remove('palette3');
+    info.classList.remove('palette3');
+    socials.classList.remove('palette3');
+  }
+}
+
+function handleRadio3(event) {
+  console.log('somos las mejores');
+  background.classList.add('palette3');
+  info.classList.add('palette3');
+  socials.classList.add('palette3');
+}
+
+//EVENTOS PALETTES
+radio1.addEventListener('click', handleRadio1);
+radio2.addEventListener('click', handleRadio2);
+radio3.addEventListener('click', handleRadio3);
