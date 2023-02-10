35cf5bc5bbf231d2c1188f0fa9c456d4734deee4
VanesaGR <vanesaglezrguez@gmail.com>
2023-02-10 12:36:29 +0100

    collapsable arrows

--- src/js/00main.js
+++ src/js/00main.js
@@ -19,16 +19,30 @@ const share = document.querySelector('.js-share');
 
 
 // FUNCIONES COLLAPSABLES
+//SE PUEDE HACER CON ARRAYS Y BUCLES. ESPERAR
 function handleDesign(event) {
   fill.classList.add('collapsable');
   share.classList.add('collapsable');
   design.classList.remove('collapsable');
+  up1.classList.remove('collapsable');
+  down1.classList.add('collapsable');
+  up3.classList.add('collapsable');
+  down3.classList.remove('collapsable');
+  up2.classList.add('collapsable');
+  down2.classList.remove('collapsable');
+
 }
 
 function handleFill(event) {
   design.classList.add('collapsable');
   share.classList.add('collapsable');
   fill.classList.remove('collapsable');
+  up2.classList.remove('collapsable');
+  down2.classList.add('collapsable');
+  up1.classList.add('collapsable');
+  down1.classList.remove('collapsable');
+  up3.classList.add('collapsable');
+  down3.classList.remove('collapsable');
 
 }
 
@@ -36,6 +50,13 @@ function handleShare(event) {
   design.classList.add('collapsable');
   fill.classList.add('collapsable');
   share.classList.remove('collapsable');
+  up3.classList.remove('collapsable');
+  down3.classList.add('collapsable');
+  up2.classList.add('collapsable');
+  down2.classList.remove('collapsable');
+  up1.classList.add('collapsable');
+  down1.classList.remove('collapsable');
+
 }
 
 // EVENTOS COLLAPSABLES
--- src/js/01variables.js
+++ src/js/01variables.js
@@ -1,1 +1,3 @@
 /* eslint-disable strict */
+
+
