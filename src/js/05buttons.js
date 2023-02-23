bd8548441c9935c9f26efec3e6e1d394948df6b1
beafig <beafigueroa89@gmail.com>
2023-02-23 11:43:21 +0100

    New run docs

--- src/js/05buttons.js
+++ src/js/05buttons.js
@@ -13,7 +13,6 @@ function handleReset(event) {
     linkedin: '',
     github: ''
   };
-  handleInputs();
   resetInputs();
   resetImage();
   resetPalette();
@@ -21,7 +20,7 @@ function handleReset(event) {
 }
 
 function resetImage() {
-  profileImage.style.backgroundImage = `url(./assets/images/profile-pic.jpg)`;
+  profileImage.style.backgroundImage = `url(../images/profile-pic.jpg)`;
   profilePreview.style.backgroundImage = '';
 }
 
